0f33e54ec5f0f706904063be7eba18a40b783090
harmon1c <80057115+harmon1c@users.noreply.github.com>
2023-10-05 14:36:42 +0300

    feat: add solution 2023-10-05 14:36:18

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  const numStr = Math.abs(n).toString();
+  let reversedString = "";
+  for (let i = numStr.length - 1;  i >= 0; i--) {
+    reversedString += numStr[i];
+  }
+  const strToNum = parseInt(reversedString, 10);
+  return strToNum;
 }
